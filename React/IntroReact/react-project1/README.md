3a9cceb6b706e0396cbff2cc8fda772bf7b7dcd5
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2025-09-27 16:40:42 +0545

    React learned at first day

--- React/IntroReact/react-project1/README.md
+++
@@ -1,3 +0,0 @@
-In src the main code actauly works
-
-1.So in assets if we have to keep any fonts mage videos,css file the we can keep it in assets
---
+++ Reactlearn/react1/src/main.jsx
@@ -0,0 +1,14 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './App.jsx'
+// import navbar from './component/navbar.jsx'
+import Navbar from './component/Navbar.jsx'
+
+createRoot(document.getElementById('root')).render(
+  
+    // <App />
+    <Navbar />
+  
+  
+)
