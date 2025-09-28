19b4f2d57c1f7bd3a31dcb80aa8e697cb4b058e0
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2025-09-28 09:09:14 +0545

    Learned to use props

--- Reactlearn/react2/src/App.jsx
+++ Reactlearn/react2/src/App.jsx
@@ -1,6 +1,9 @@
 
 import './App.css'
 import UserCard from './components/UserCard'
+import spin from './assets/spin.png'
+import hello from './assets/hello.jpg'
+import gpt from './assets/gpt.png'
 
 function App() {
  
@@ -8,9 +11,9 @@ function App() {
     <>
     <div className='container'>
 
-    <UserCard/>
-    <UserCard/>
-    <UserCard/>
+    <UserCard name="Anmol Sunar" desc="k xa dost" image={spin}/>
+    <UserCard name="Sabina Sunar" desc="Thik xa sab" image={gpt} style={{"border-radius":"10px"}}/>
+    <UserCard name="Barsha Sunar"  desc="hora" image={hello}/>
         
     </div>
     {/* <div>
--- Reactlearn/react2/src/components/UserCard.jsx
+++ Reactlearn/react2/src/components/UserCard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import spin from '../assets/spin.png'
 import './UserCard.css'
 
-const UserCard = () => {
+const UserCard = (props) => {
   return (
-    <div className='user-container'>
+    <div className='user-container' style={props.style}>
 
-        <p id='user-title'>MIjash Sunar</p>
+        <p id='user-title'>{props.name}</p>
         {/* <img id='user-img' src="../assets/sping.png"/> This is not the way we need to import it */}
-                <img id='user-img' src={spin}/>
-        <p id='user-desc'>Description</p>
+                <img id='user-img' src={props.image}/>
+        <p id='user-desc'>{props.desc}</p>
 
 
       
@@ -19,4 +19,7 @@ const UserCard = () => {
 
 export default UserCard
 
-// In jsx there is no class there is className
+// In jsx there is no class there is classNameo
+//Props are exported and imported in  other componet
+// JSx is the file where we can write html code and javascript code easily there
+// In js file if w ehave to write hmtl code the we should have jsx file
