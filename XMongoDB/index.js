2ea57a2956ba7d34a4fa8ccabe19f4dfc0e35f4b
mijashsunar10 <mijashsunar.xdezo@gmail.com>
2025-10-13 14:46:15 +0545

    MongoDb learnt a bit

---
+++ XMongoDB/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const connectDB = require('./db');
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
